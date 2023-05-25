69114ed9b1bbc85b0b1d9a26908d0b0a24f58994
Anton A <zantshark@gmailcom>
2023-05-25 11:54:03 +0100

    The first push

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# Проект парсинга pep
+# PEP parsing service
+
+## Description
+This project parses information about PEP documentation from the Python website
+It has several modes and output options.
+
+## Run service:
+To run the service, use the command with preferred options:
+```
+python main.py [mode] [options]
+```
+The service has the next modes:
+```
+whats-new  # shows the last changelog for the latest Python release
+latest-versions  # shows statuses of the latest Python versions
+download  # downloads an archive containing all the documents for the version of Python
+pep  # shows status information for the all PEP documents
+```
+
+The service has the next options:
+```
+-c | --clear-cache  # clears the cache before parsing precess
+-o pretty | --output pretty  # prints the result in an attractive ASCII form
+-o pretty | --output file  # write the result in a .csv file
+```
+
+## Technologies
+Python 3
+BeautifulSoup4
+
+## The author of the project
+Anton Akulov - https://github.com/Nekustetnaz
---
+++ src/utils.py
@@ -0,0 +1,35 @@
+import logging
+
+from requests import RequestException
+
+from exceptions import ParserFindTagException
+
+
+def get_response(session, url):
+    try:
+        response = session.get(url)
+        response.encoding = 'utf-8'
+        return response
+    except RequestException:
+        logging.exception(
+            f'Возникла ошибка при загрузке страницы {url}',
+            stack_info=True
+        )
+
+
+def find_tag(soup, tag, attrs=None):
+    searched_tag = soup.find(tag, attrs=(attrs or {}))
+    if searched_tag is None:
+        error_msg = f'Не найден тег {tag} {attrs}'
+        logging.error(error_msg, stack_info=True)
+        raise ParserFindTagException(error_msg)
+    return searched_tag
+
+
+def find_sibling(soup):
+    searched_sibling = soup.find_next_sibling()
+    if searched_sibling is None:
+        error_msg = 'Метод "find_next_siblings" не нашёл следующий тег'
+        logging.error(error_msg, stack_info=True)
+        raise ParserFindTagException(error_msg)
+    return searched_sibling
